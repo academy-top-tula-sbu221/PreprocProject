953485a4290d3d13195f3c15be3401ee50b00f8b
maxefimov <efimov.academy.top@gmail.com>
2023-04-08 11:07:10 +0300

    Добавьте файлы проекта.

---
+++ PreprocProject/Libs.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void Action()
+{
+	std::cout << "Acrtion\n";
+}
---
+++ PreprocProject/PreprocProject.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+#define PI 3.1416
+
+#define SQR(A) (A) * (A)
+
+#define X64
+
+#ifdef X64
+int a;
+#endif
+
+using namespace std;
+
+const float pi = 3.1416;
+
+
+void Action();
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    cout << 10 * PI << "\n";
+    cout << SQR(10 + 20) << "\n"; 
+}
